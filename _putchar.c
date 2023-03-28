dfa228c31dc6cb8fb426e8579957e5595b6f7ee6
larrybah <larrybah@gmail.com>
2023-03-28 10:14:38 +0000

    printf

--- _putchar.c
+++ _putchar.c
@@ -1,9 +1,11 @@
 #include <unistd.h>
+
 /**
  * _putchar - prints a single character
- * @c: the character
+ * @c: parameter the character to be printed
  * Return: 1
  */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
